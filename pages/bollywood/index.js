0c559ac5369a4b2ddc4bfdba9e4dd23f922bbc96
vikas <vikaskky@gmail.com>
2023-08-14 09:37:43 +0530

    add url in sitemap

--- pages/bollywood/index.js
+++ pages/bollywood/index.js
@@ -5,43 +5,24 @@ const catType = {
   popularMovies: 'movie/popular',
   nowPlaying:'movie/now_playing'
 }
-const MOVIEAPI = `https://api.themoviedb.org/3/${catType.discoverMovie}?api_key=10682f9f7e873f9fefa9c47949aca414`;
-const payload = {
-  certification_country: "IN",
-  with_original_language: "hi",
-  "release_date.gte": moment().subtract(1, 'year').startOf('year').format("YYYY-MM-DD") ||"2023-01-01",
-  "release_date.lte": moment().add(1, 'week').format("YYYY-MM-DD") || "2023-08-16",
-}
-
-
-const bollywoodMovieUrl =
-  `${MOVIEAPI}&with_original_language=${payload.with_original_language}&certification_country=${payload.certification_country}&release_date.gte=${payload['release_date.gte']}&release_date.lte=${payload['release_date.lte']}`
 import Home from "..";
 import moment from 'moment/moment';
 import { getDiscoverMovies } from '../../api/movie';
+
+const payload = {
+  certification_country: "IN",
+  with_original_language: "hi",
+  "release_date.gte": moment().subtract(1, 'year').startOf('year').format("YYYY-MM-DD") || "2023-01-01",
+  "release_date.lte": moment().add(1, 'week').format("YYYY-MM-DD") || "2023-08-16",
+}
 
 export default Home;
 
 
 export async function getStaticProps() {
   // const session = await getSession(context);
-  // const [
-  //   popularMoviesRes,
-  //   popularMoviesRes2,
-  //   popularMoviesRes3
-  // ] = await Promise.all([
-  //   getDiscoverMovies({ ...payload, page: 1 }),
-  //   getDiscoverMovies({ ...payload, page: 2 }),
-  //   getDiscoverMovies({ ...payload, page: 3 }),
-  //   // fetch(`${bollywoodMovieUrl}&page=1`),
-  //   // fetch(`${bollywoodMovieUrl}&page=2`),
-  //   // fetch(`${bollywoodMovieUrl}&page=3`),
-  // ]);
   const [Movies1, MovieList2, MovieList3=[]] =
     await Promise.all([
-      // popularMoviesRes.json(),
-      // popularMoviesRes2.json(),
-      // popularMoviesRes3.json(),
       getDiscoverMovies({ ...payload, page: 1 }),
       getDiscoverMovies({ ...payload, page: 2 }),
       getDiscoverMovies({ ...payload, page: 3 }),
