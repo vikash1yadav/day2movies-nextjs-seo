e35b75d366e87aaedcc7dd7966c46ec5cd5d0e6f
vikas <vikaskky@gmail.com>
2023-08-15 10:50:14 +0530

    seo optimization

--- components/MovieThumbnail.js
+++ components/MovieThumbnail.js
@@ -1,10 +1,9 @@
-import Image from "next/image";
-import { useRouter } from "next/router";
 import slugify from "../utils/slugify";
+import Link from 'next/link';
 
 function MovieThumbnail({ result }) {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
-  const router = useRouter();
+  // const router = useRouter();
   const checkTvOrMovieFromTitle = (original_title, original_name) => {
     if (original_title) {
       return "movie";
@@ -12,15 +11,16 @@ function MovieThumbnail({ result }) {
       return "series";
     }
   };
-
+  const slugifyUrl = `/${checkTvOrMovieFromTitle(result.original_title, result.original_name)}/${checkTvOrMovieFromTitle(result.original_title, result.original_name) == "movie" ? slugify(result.original_title) : slugify(result.original_name)}/${result.id}`;
   return (
     <div
       // className="postItem"
       // tailwind w-[190px] h-[330px]
       className=" bg-[#282c34] text-[white] cursor-pointer m-[5px] p-[5px] rounded-[10px] hover:bg-[white] hover:text-[black]"
       // onClick={() => router.push(`/movie/${result.id}`)}
-      onClick={() => router.push(`/${checkTvOrMovieFromTitle(result.original_title, result.original_name)}/${checkTvOrMovieFromTitle(result.original_title, result.original_name) == "movie" ? slugify(result.original_title) : slugify(result.original_name)}/${result.id}`)}
+      // onClick={() => router.push()}
     >
+      <Link href={slugifyUrl}>
       <div
         className="link"
         to={`/movie/${result.id}`}
@@ -45,7 +45,8 @@ function MovieThumbnail({ result }) {
             <p style={{ display: "none" }}>{result.overview}</p>
           </div>
         </div>
-      </div>
+        </div>
+      </Link>
     </div>
   );
 }
--- components/ShowThumbnail.js
+++ components/ShowThumbnail.js
@@ -1,5 +1,7 @@
+import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import slugify from "../utils/slugify";
 
 function ShowThumbnail({ result }) {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
@@ -13,9 +15,7 @@ function ShowThumbnail({ result }) {
     }
   };
 
-  const reformatTitle = (title) => {
-    return title.replaceAll(" ", "-").toLowerCase();
-  };
+  const slugifyUrl = `/${checkTvOrMovieFromTitle(result.original_title, result.original_name)}/${checkTvOrMovieFromTitle(result.original_title, result.original_name) == "movie" ? slugify(result.original_title) : slugify(result.original_name)}/${result.id}`;
   return (
     <>
     <div
@@ -26,9 +26,9 @@ function ShowThumbnail({ result }) {
         // min-w-[20vw]
         // md:min-w-[200px] md:min-h-[330px]
           // className="flex min-w-[250px] min-h-[170px] md:min-w-[330px] md:min-h-[210px] rounded-lg overflow-hidden shadow-xl cursor-pointer border-[3px] border-[#f9f9f9] border-opacity-10  hover:border-opacity-80 hover:shadow-2xl transform hover:scale-105 transition duration-300"
-        onClick={() => router.push(`/${checkTvOrMovieFromTitle(result.original_title, result.original_name)}/${checkTvOrMovieFromTitle(result.original_title, result.original_name) == "movie" ? reformatTitle(result.original_title) : reformatTitle(result.original_name)}/${result.id}`)}
+        // onClick={() => router.push()}
         >
-
+        <Link href={slugifyUrl}></Link>
       <Image
         src={
           `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
@@ -50,34 +50,6 @@ function ShowThumbnail({ result }) {
           </div>
           
         </div>
-
-      {/* <div
-        className="postItem"
-        onClick={() => router.push(`/${checkTvOrMovieFromTitle(result.original_title, result.original_name)}/${checkTvOrMovieFromTitle(result.original_title, result.original_name) == "movie" ? reformatTitle(result.original_title) : reformatTitle(result.original_name)}/${result.id}`)}
-      >
-        <div
-          className="link"
-          to={`/movie/${result.id}`}
-          style={{ textDecoration: "none", color: "inherit" }}
-        >
-          <img
-            className="postImg"
-            src={`https://image.tmdb.org/t/p/w780/${result.poster_path}`}
-            // alt={content.title + ", day2movies , watch latest movie online for free on day2movies"}
-            title={result.title || result.original_name + " day2movies"}
-          />
-
-          <div className="postInfo">
-            <span className="postTitle">
-              {result.title || result.original_name}
-            </span>
-            <div className="movieDetails">
-              <p className="movieDate">{result.release_date}</p>
-              <p className="type">{result.vote_average}</p>
-            </div>
-          </div>
-        </div>
-      </div> */}
     </>
   );
 }
