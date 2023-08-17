54a602535fc315230970b76bf52e116860b3e108
vikash1yadav <64723750+vikash1yadav@users.noreply.github.com>
2023-08-17 19:40:49 +0000

    fresh upload from next-03

---
+++ src/components/carousel/customSlide.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { useRouter } from "next/navigation";
+import Const from "../../helper/constant";
+import slugify from "../../../utils/slugify";
+
+// //import "./style.css";
+
+export default function CustomSlide(props) {
+  const { backdrop, title, overview, genres, id, original_title, original_name, ...otherprops } = props; 
+  const router = useRouter();
+  const checkTvOrMovieFromTitle = (original_title, original_name) => {
+    if (original_title) {
+      return "movie";
+    } else if (original_name) {
+      return "series";
+    }
+  };
+
+  const reformatTitle = (title) => {
+    return slugify(title) ||title.replace(/ /g, "-").toLowerCase();
+  };
+  const getCurrentUrl = `/${checkTvOrMovieFromTitle(original_title, original_name)}/${checkTvOrMovieFromTitle(original_title, original_name) == "movie" ? reformatTitle(title) : reformatTitle(props?.name)}/${id}`;
+  return (
+    <div {...otherprops}>
+      <div className="mx-1 mx-lg-1 mx-xl-2 cursor-pointer carousel-container"
+        onClick={() => router.push(getCurrentUrl)}
+      >
+        <div className="carousel-description">
+          <h2>{title}</h2>
+          {/* <h6>{genres.slice(0, 3).join(" . ")}</h6> */}
+          <p>{overview}</p>
+        </div>
+        <div className="carousel-overlay"></div>
+        <div className="carousel-img">
+          <img
+            src={`https://image.tmdb.org/t/p/w1280${backdrop}`}
+            title={title||"day2movies"}
+            alt={Const.ATTRIBUTES.IMG}
+            className="d-inline"
+            loading="lazy"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/helper/constant.js
@@ -0,0 +1,35 @@
+// require('dotenv').config();
+export default {
+    DAY2MOVIE_URL: process.env.DAY2MOVIE_LINK,
+    TMDB_API_KEY: process.env.API_KEY,
+    TMDB: {
+        API_BASE_URL: "https://api.themoviedb.org/3",
+        // API_KEY: "10682f9f7e873f9fefa9c47949aca414",
+        API_KEY: process.env.API_KEY || "10682f9f7e873f9fefa9c47949aca414",
+        AUTH_KEY: process.env.TMDB_AUTH_KEY,
+        IMAGE_BASE_URL: 'https://image.tmdb.org/t/p',
+        PAGE_LIMIT : 20,
+    },
+    STREAM_SERVER: {
+        GD_STREAM_BASE_URL: "https://databasegdriveplayer.xyz/player.php",
+        YOUTUBE_BASE_URL:"https://www.youtube.com/watch",
+    },
+    TMDB_CAT_BASED_URL : {
+        tendingAllByWeek: 'trending/all/week',
+        DISCOVER_MOVIE: 'discover/movie',
+        popularMovies: 'movie/popular',
+        nowPlaying: 'movie/now_playing',
+        topRated:"movie/top_rated",
+        MOVIE: "movie",
+        TV:"tv",
+        POPULAR_SHOW:"tv/popular",
+        TOP_RATED_SHOW: "tv/top_rated"
+    },
+    DATE_FORMAT: {
+        API_DATE:"YYYY-MM-DD"
+    },
+    ATTRIBUTES: {
+      IMG:"day2movies"  
+    }
+
+}
