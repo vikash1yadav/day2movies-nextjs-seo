0dd5c79baf6c5b4407322fa96246079892ee36e9
vikas <vikaskky@gmail.com>
2023-08-21 18:58:36 +0530

    added alt for logo

--- src/components/header/AppHeading.js
+++ src/components/header/AppHeading.js
@@ -1,9 +1,10 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import constant from '@/helper/constant';
 import NextLink from 'next/link';
 
 const AppHeading = () => (
   <NextLink href="/" className="flex flex-col text-2xl font-bold ml-5 mr-8 py-3 text-textHighlight">
-    <img src='/images/day2movies-icon.png' className='h-12 w-28' />
+    <img src='/images/day2movies-icon.png' alt={constant.ATTRIBUTES.IMG} className='h-12 w-28' />
     {/* Day2Movies
     <span className='text-[10px] text-center pt-0'>stream day2night</span> */}
   </NextLink>
