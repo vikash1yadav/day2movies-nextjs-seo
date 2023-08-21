28dcb8ac9b4c5a8a7a20ba7db6606c9bcd081757
vikas <vikaskky@gmail.com>
2023-08-21 12:08:42 +0530

    otpimize img for slider for small and large screen

--- src/components/carousel/customSlide.js
+++ src/components/carousel/customSlide.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Const from "../../helper/constant";
 import slugify from "../../../utils/slugify";
 
 export default function CustomSlide(props) {
   const { backdrop, title, overview, genres, id, original_title, original_name, ...otherprops } = props; 
-  const IMG_URL = `${Const.TMDB.IMAGE_BASE_URL}/${Const.SLIDER_IMG_SIZE}${backdrop}`
+  const [img_size, setImg_size] = useState(Const.SLIDER_IMG_SIZE);
+  const IMG_URL = `${Const.TMDB.IMAGE_BASE_URL}/${img_size}${backdrop}`
   const router = useRouter();
   const checkTvOrMovieFromTitle = (original_title, original_name) => {
     if (original_title) {
@@ -18,6 +19,19 @@ export default function CustomSlide(props) {
     return slugify(title) ||title.replace(/ /g, "-").toLowerCase();
   };
   const getCurrentUrl = `/${checkTvOrMovieFromTitle(original_title, original_name)}/${checkTvOrMovieFromTitle(original_title, original_name) == "movie" ? reformatTitle(title) : reformatTitle(props?.name)}/${id}`;
+  useEffect(() => {
+    const screen_size = window.screen.width;
+    if (screen_size <= 500) {
+      setImg_size('w300');
+    }
+    else if (screen_size <= 700) {
+      setImg_size('w500');
+    } else if (screen_size <=1200) {
+      setImg_size('w780');
+    } {
+      setImg_size('w1280');
+    }
+  },)
   return (
     <div {...otherprops}>
       <div className="mx-1 mx-lg-1 mx-xl-2 cursor-pointer carousel-container"
