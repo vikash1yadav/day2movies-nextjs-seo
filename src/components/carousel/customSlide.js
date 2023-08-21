d052a8bab58ad77af5d2219d5db743c621d7f8f5
vikas <vikaskky@gmail.com>
2023-08-21 13:50:24 +0530

    otpimize img for slider for small and large screen by using useEffect

--- src/components/carousel/customSlide.js
+++ src/components/carousel/customSlide.js
@@ -5,8 +5,7 @@ import slugify from "../../../utils/slugify";
 
 export default function CustomSlide(props) {
   const { backdrop, title, overview, genres, id, original_title, original_name, ...otherprops } = props; 
-  const [img_size, setImg_size] = useState(Const.SLIDER_IMG_SIZE);
-  const IMG_URL = `${Const.TMDB.IMAGE_BASE_URL}/${img_size}${backdrop}`
+  const [imgUrl, setImgUrl] = useState(`${Const.TMDB.IMAGE_BASE_URL}/w780${backdrop}`);
   const router = useRouter();
   const checkTvOrMovieFromTitle = (original_title, original_name) => {
     if (original_title) {
@@ -20,18 +19,38 @@ export default function CustomSlide(props) {
   };
   const getCurrentUrl = `/${checkTvOrMovieFromTitle(original_title, original_name)}/${checkTvOrMovieFromTitle(original_title, original_name) == "movie" ? reformatTitle(title) : reformatTitle(props?.name)}/${id}`;
   useEffect(() => {
+    let img_size = Const.SLIDER_IMG_SIZE;
     const screen_size = window.screen.width;
-    if (screen_size <= 500) {
-      setImg_size('w300');
+    const clientBodyWidth = document.body.clientWidth;
+    let IMG_URL;
+    if (clientBodyWidth <= 500) {
+      console.log(1);
+      img_size = 'w300';
+      IMG_URL = `${Const.TMDB.IMAGE_BASE_URL}/${img_size}${backdrop}`
+      setImgUrl(IMG_URL);
     }
     else if (screen_size <= 700) {
-      setImg_size('w500');
+      console.log(2);
+      img_size = 'w500';
+      IMG_URL = `${Const.TMDB.IMAGE_BASE_URL}/${img_size}${backdrop}`
+      setImgUrl(IMG_URL);
+      return;
     } else if (screen_size <=1200) {
-      setImg_size('w780');
-    } {
-      setImg_size('w1280');
+      console.log(3);
+      img_size = 'w780';
+      IMG_URL = `${Const.TMDB.IMAGE_BASE_URL}/${img_size}${backdrop}`
+      setImgUrl(IMG_URL);
+      return;
+    } else {
+      console.log(4);
+      img_size = 'w1280';
+      IMG_URL = `${Const.TMDB.IMAGE_BASE_URL}/${img_size}${backdrop}`
+      setImgUrl(IMG_URL);
+      return;
     }
-  },)
+    console.log("width", imgUrl, clientBodyWidth);
+  }, [])
+
   return (
     <div {...otherprops}>
       <div className="mx-1 mx-lg-1 mx-xl-2 cursor-pointer carousel-container"
@@ -45,7 +64,7 @@ export default function CustomSlide(props) {
         <div className="carousel-overlay"></div>
         <div className="carousel-img">
           <img
-            src={IMG_URL}
+            src={imgUrl}
             title={title||"day2movies"}
             alt={Const.ATTRIBUTES.IMG}
             className="d-inline"
--- src/components/header/AppHeading.js
+++ src/components/header/AppHeading.js
@@ -2,8 +2,9 @@
 import NextLink from 'next/link';
 
 const AppHeading = () => (
-  <NextLink href="/" className="text-2xl font-bold ml-5 mr-8 py-5 text-textHighlight">
-    Day2movies
+  <NextLink href="/" className="text-2xl font-bold ml-5 mr-8 py-5 flex text-textHighlight">
+    {/* <img src='/images/day2movies-icon.png' className='w-[90px]' /> */}
+    Day2Movies
   </NextLink>
 );
 
