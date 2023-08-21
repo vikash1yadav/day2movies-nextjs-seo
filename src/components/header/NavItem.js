dc7f7873b91013753923f49d72bf301f8f6ea5e7
vikas <vikaskky@gmail.com>
2023-08-21 18:30:52 +0530

    disable auto mode for now

--- src/components/header/NavItem.js
+++ src/components/header/NavItem.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import NextLink from 'next/link';
-
+// import seriesIcon from '../vector/series-icon';
 // interface NavItemProps {
 //   name: 'TV' | 'Movies';
 // }
@@ -10,6 +10,7 @@ const NavItem = ({ name, icon_url, link }) => (
     href={`/${link}`}
     className="flex items-center text-xl sm:text-lg hover:text-textHighlight font-semibold tracking-wide pl-5 py-3 sm:py-3 sm:mr-5">
     {icon_url && <img src={icon_url} alt="" className="h-5 px-1" />}
+    {/* <seriesIcon className='h-10 px-1 text-black' /> */}
     {name}
 
   </NextLink>)
---
+++ src/components/vector/series-icon.js
@@ -0,0 +1,22 @@
+import React from "react";
+function seriesIcon(props) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="#fff"
+            ariaHidden="true"
+            ariaLabel="series"
+            color="#fff"
+            data-route="SERIES"
+            viewBox="0 0 36 36"
+            {...props}
+        >
+            <path
+                d="M18.84 11.965h6.722a4 4 0 014 4V26a4 4 0 01-4 4H10.375a4 4 0 01-4-4V15.965a4 4 0 014-4h6.211l-3.981-3.981a1.162 1.162 0 111.643-1.644l3.465 3.465 3.464-3.465a1.162 1.162 0 011.644 1.644l-3.982 3.981zm6.428 7.73a1.718 1.718 0 100-3.436 1.718 1.718 0 000 3.436zm0 6.011a1.718 1.718 0 100-3.435 1.718 1.718 0 000 3.435z"
+                className="sc-kAzzGY dGwULW"
+            ></path>
+        </svg>
+    );
+}
+
+export default seriesIcon;
